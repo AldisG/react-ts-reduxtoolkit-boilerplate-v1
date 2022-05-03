95dabfe2f64e5c1b02fdad53f6cc7f9f8668a6cd
Aldis Gailis <aldis.gailis@28stone.com>
2022-05-03 12:34:29 +0300

    small updates

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -45,6 +45,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '..', './src/index.html'),
+      favicon: "./src/favicon.ico"
     }),
   ],
 }
